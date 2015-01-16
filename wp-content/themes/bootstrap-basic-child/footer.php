63f0b23dd3711b4661f11efe7b5eb95d4edcf5b0
Josh M. Diaz <joshua.michael.diaz@gmail.com>
2015-01-15 17:48:56 -0700

    Changed the footer to reflect UVU. Added login.

--- wp-content/themes/bootstrap-basic-child/footer.php
+++ wp-content/themes/bootstrap-basic-child/footer.php
@@ -1,34 +1,30 @@
 <?php
 /**
  * The theme footer
- * 
+ *
  * @package bootstrap-basic
  */
 ?>
 
 			</div><!--.site-content-->
-			
-			
+
+
 			<footer id="site-footer" role="contentinfo">
 				<div id="footer-row" class="row site-footer">
 					<div class="col-md-6 footer-left">
-						<?php 
-						if (!dynamic_sidebar('footer-left')) {
-							printf(__('Powered by %s', 'bootstrap-basic'), 'WordPress');
-							echo ' | ';
-							printf(__('Theme: %s', 'bootstrap-basic'), '<a href="http://okvee.net">Bootstrap Basic</a>');
-						} 
-						?> 
+						<p>
+							Special Olympics Utah | Created By: Utah Valley University | <a href="http://sout.caitlinherzog.com/wp-admin">Login</a>
+						</p>
 					</div>
 					<div class="col-md-6 footer-right text-right">
-						<?php dynamic_sidebar('footer-right'); ?> 
+						<?php dynamic_sidebar('footer-right'); ?>
 					</div>
 				</div>
 			</footer>
 		</div><!--.container page-container-->
-		
-		
+
+
 		<!--wordpress footer-->
-		<?php wp_footer(); ?> 
+		<?php wp_footer(); ?>
 	</body>
 </html>
